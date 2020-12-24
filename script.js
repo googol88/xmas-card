95ec16922ccd54ddebf189a24422c6dddd44bb1c
googol88 <ronaldfleung@gmail.com>
2020-12-23 20:29:15 -0500

    remove extraneous comments

--- script.js
+++ script.js
@@ -14,27 +14,3 @@ snowStorm.useMeltEffect = false;
 snowStorm.freezeOnBlur = false;
 snowStorm.vMaxX = 0;
 snowStorm.vMaxY = 2;
-/*(function() {
-    let canvas = document.getElementById('confetti-canvas'),
-            context = canvas.getContext('2d');
-
-    // resize the canvas to fill browser window dynamically
-    window.addEventListener('resize', resizeCanvas, false);
-
-    function resizeCanvas() {
-            canvas.width = window.innerWidth;
-            canvas.height = window.innerHeight;
-
-
-            drawStuff(); 
-    }
-    resizeCanvas();
-
-    function drawStuff() {
-            // do your drawing stuff here
-          let confettiSettings = {"target":canvas,"max":"128","size":"1","animate":true,"props":["circle","square","triangle","line"],"colors":[[165,104,246],[230,61,135],[0,199,228],[253,214,126]],"clock":"16","rotate":true,"width":"","height":"","start_from_edge":false,"respawn":true};
-      
-      let confetti = new ConfettiGenerator(confettiSettings);
-            confetti.render();
-    }
-})();*/
